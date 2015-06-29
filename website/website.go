1243622dd6a9370b21f42767de375d3433f084fc
Vincent Aube <vincent.aube@hotmail.com>
2015-06-28 21:30:15 -0400

    Trying the default http + gorilla mux for a little website

--- website/website.go
+++ website/website.go
@@ -1,1 +1,35 @@
-package website
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+const (
+	index string = "index.html"
+)
+
+var templates = template.Must(template.ParseFiles(staticResource(index)))
+
+func main() {
+	r := mux.NewRouter().StrictSlash(false)
+	r.NotFoundHandler = http.HandlerFunc(notFound)
+	r.HandleFunc("/", defaultHandler)
+
+	http.ListenAndServe(":8080", r)
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Page not found!")
+}
+
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, index, nil)
+}
+
+func staticResource(name string) string {
+	return fmt.Sprintf("./static/%s", name)
+}
